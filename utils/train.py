1f01a0b0fca90a4f9bbe24517bdc30e1db841dc4
binnz <binnz>
2020-05-31 13:33:17 +0800

    fix pbar

--- utils/train.py
+++ utils/train.py
@@ -14,19 +14,14 @@ def one_cycle(epoch, config, model, optimizer, scheduler, criterion, data_loader
 
     with tqdm(total=len(data_loader), desc=f'Epoch: {epoch + 1}') as pbar:
         for i, data in enumerate(data_loader):
-            print("Time 0", time.time())
             batch = Batch(data, device, pad=tokenizer.pad_token_id)
             out = model(batch.source, batch.source_mask,
                             batch.target, batch.target_mask, batch.utter_type)
-            print("Input", batch.source)
-            print("Target", batch.target)
-            print("Time 1", time.time())
             loss = criterion(out.transpose(1, 2), batch.target_y).mean()
             loss = loss / config.gradient_accumulation_steps
             loss.backward()
             print("Loss", loss)
             logger.log_value('loss', loss.item(), epoch*len(data_loader) + i)
-            print("Time 2", time.time())
             if (i + 1) % config.gradient_accumulation_steps == 0:
                 clip_grad_norm_(filter(lambda p: p.requires_grad, model.parameters()), config.max_grad_norm)
                 optimizer.step()
